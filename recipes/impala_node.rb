3d138bbfdf8b90c31a861a71fadc88cbddb21842
Isaac <icylisper@gmail.com>
2013-06-04 14:20:51 +0530

    Copy hdfs config files to impala config dir

--- recipes/impala_node.rb
+++ recipes/impala_node.rb
@@ -1,6 +1,10 @@
 
 include_recipe 'hadoop_cluster::impala' 
 
+%w[ core-site.xml hdfs-site.xml].each do |conf_file|
+  execute "cp /etc/hadoop/conf/#{conf_file} /etc/impala/conf/"
+end
+
 package "impala-server" do
   action :install
 end
