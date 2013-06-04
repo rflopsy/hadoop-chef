9a74bb9ae6af350ec4e603369e2426239dd78e9d
Isaac <icylisper@gmail.com>
2013-06-04 14:42:17 +0530

    Fix starting of impala server on the node

--- recipes/impala_node.rb
+++ recipes/impala_node.rb
@@ -1,11 +1,27 @@
 
 include_recipe 'hadoop_cluster::impala' 
 
-%w[ core-site.xml hdfs-site.xml].each do |conf_file|
+%w[core-site.xml hdfs-site.xml].each do |conf_file|
   execute "cp /etc/hadoop/conf/#{conf_file} /etc/impala/conf/"
 end
+
 
 package "impala-server" do
   action :install
 end
+
+namenode  = discover(:namenode, :server).private_ip rescue nil
+
+site_variables = {
+    :state_store  => namenode,
+}
+
+template "/etc/default/impala" do
+  source "default-impala.erb"
+  mode 0664
+  owner "root"
+  variables(site_variables)
+end
+
+execute("/etc/init.d/impala-server start")
 
