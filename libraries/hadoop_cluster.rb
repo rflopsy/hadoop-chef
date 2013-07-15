10d6d4722d51b9b7bfd5bafb5f2a89a45ee8db1d
Abhijith <abhijithg@gmail.com>
2013-07-15 10:29:56 +0530

    use 3 disks for hadoop dirs

--- libraries/hadoop_cluster.rb
+++ libraries/hadoop_cluster.rb
@@ -53,11 +53,11 @@ module HadoopCluster
   end
 
   def local_hadoop_dirs
-    ["/mnt2/hadoop"]
+    ["/mnt1/hadoop", "/mnt2/hadoop", "/mnt3/hadoop"]
   end
 
   def persistent_hadoop_dirs
-    ["/mnt2/hadoop"]
+    ["/mnt1/hadoop", "/mnt2/hadoop", "/mnt3/hadoop"]
   end
   
   def cluster_ebs_volumes_are_mounted?
